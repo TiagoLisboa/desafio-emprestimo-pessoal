33acae73637dc8174529e0a3b9741675b77aa8f5
Tiago Lisboa <tiago.lisboa@pitang.com>
2023-06-20 14:42:22 -0300

    Create ProposalFields model

--- emprestimo/proposta/models.py
+++ emprestimo/proposta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ProposalField(models.Model):
+    name = models.CharField(max_length=200)
+    field_type = models.IntegerField(choices=[
+        (0, 'string'),
+        (1, 'numeric'),
+        (2, 'boolean'),
+    ])
+    is_required = models.BooleanField()
+
