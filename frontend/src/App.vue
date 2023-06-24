a7da2cf10dae55281d4c41c0245cec5e6e4f02e2
Tiago Lisboa <tiago.lisboa@pitang.com>
2023-06-23 21:31:08 -0300

    Submit proposal form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,61 @@
 <script setup lang="ts">
-    import axios from 'axios'
-    import { ref } from 'vue'
+    import axios from 'axios';
+    import { ref } from 'vue';
 
     const proposalFields = ref([]);
-    const fieldRefs = ref([])
-
+    const fieldRefs = ref([]);
+    const answers = ref({})
+    let defaultAnswers = {}
 
     const headers = {
         'Sec-Fetch-Site': 'same-origin'
     };
+
+
     axios
         .get('http://localhost:1339/api/api/proposal_fields', headers)
-        .then(response => {proposalFields.value = response.data});
+        .then(response => {
+            function getDefaultValue(type) {
+                switch (type) {
+                    case "boolean":
+                        return false;
+                    case "numeric":
+                    case "integer":
+                        return 0;
+                    default:
+                        return "";
+                }
+            }
+            proposalFields.value = response.data;
+
+            console.log(defaultAnswers)
+            answers.value = defaultAnswers
+        });
+
+    function onSubmit(e) {
+        e.preventDefault();
+
+        axios
+            .post('http://localhost:1339/api/api/proposals', answers.value)
+            .then(response => {answers.value = defaultAnswers});
+    }
 
 </script>
 
 <template>
     <main>
-        <div v-for="field in proposalFields" ref="fieldRefs">
-            <label :for="field.id">
-                {{ field.name }}
-            </label>
-            <input v-if="field.field_type == 'string'" type="text" :name="field.name" :id="field.id" />
-            <input v-if="field.field_type == 'numeric'" type="number" :name="field.name" :id="field.id" />
-            <input v-if="field.field_type == 'integer'" type="number" step="1" :name="field.name" :id="field.id" />
-            <input v-if="field.field_type == 'boolean'" type="checkbox" :name="field.name" :id="field.id" />
-        </div>
+        <form @submit="onSubmit">
+            <div v-for="field in proposalFields" ref="fieldRefs">
+                <label :for="field.id">
+                    {{ field.name }}
+                </label>
+                <input v-if="field.field_type == 'string'" type="text" v-model="answers[field.name]" :id="field.id" />
+                <input v-if="field.field_type == 'numeric'" type="number" v-model="answers[field.name]" :id="field.id" />
+                <input v-if="field.field_type == 'integer'" type="number" step="1" v-model="answers[field.name]" :id="field.id" />
+                <input v-if="field.field_type == 'boolean'" type="checkbox" v-model="answers[field.name]" :id="field.id" />
+            </div>
+            <button>Submit</button>
+        </form>
     </main>
 </template>
 
