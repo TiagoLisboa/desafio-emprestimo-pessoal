1eff90c00b4eaf5c437041e652daea16a0fd50db
Tiago Lisboa <tiago.lisboa@pitang.com>
2023-06-23 21:13:28 -0300

    Remove authentication

--- emprestimo/api/views.py
+++ emprestimo/api/views.py
@@ -2,11 +2,17 @@ from .serializers import ProposalSerializer, ProposalFieldSerializer
 from .models import ProposalField
 from .tasks import evaluate_proposal
 
+from emprestimo.authenticators import CsrfExemptSessionAuthentication
+
 from rest_framework import mixins, generics
+from rest_framework.authentication import BasicAuthentication
+from django.views.decorators.csrf import csrf_exempt
 
 class CreateProposal(mixins.CreateModelMixin,
                      generics.GenericAPIView):
     serializer_class = ProposalSerializer
+    permission_classes = []
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 
     def post(self, request, *args, **kwargs):
         response = self.create(request, *args, **kwargs)
